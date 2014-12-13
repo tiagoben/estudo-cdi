820cd061c0d4578ebbfae294a649a1b03189b42c
Tiago Bento <tiagoben2@gmail.com>
2014-12-13 14:18:08 -0200

    Criando a classe "Oi"

--- src/cdi/servlet/OiServlet.java
+++ src/cdi/servlet/OiServlet.java
@@ -8,13 +8,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/hello")
-public class HelloServlet extends HttpServlet {
+@WebServlet("/oi")
+public class OiServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		resp.getOutputStream().print("Hello servlet!!!");
+		resp.getOutputStream().print("Oi!!!");
 	}
 
 	
